494e0dc47fd9c1b7209fd82f835f5564b81571b3
SalaiThomas <salaithomas582@gmail.com>
2024-10-13 10:49:19 +0630

    init_real_estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Real Estate",
+    "summary": "Test",
+    "version": "17.0.0.0",
+    "depends": ["base","crm"],
+    "data":[
+        "security/res_groups.xml",
+    ],
+    "demo":[
+        "demo/demo.xml",
+    ],
+}
---
+++ estate/models/real_estate.py
@@ -0,0 +1,9 @@
+from odoo import fields, models, api
+
+
+class RealEstate(models.Model):
+    _name = 'real.estate'
+    _description = 'Test model'
+
+    name = fields.Char(string="Name")
+    price = fields.Float(string="Float")
