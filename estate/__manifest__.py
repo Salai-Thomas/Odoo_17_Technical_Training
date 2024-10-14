83b85142f1642e32cb0e22c6e818e2c444ef981d
SalaiThomas <salaithomas582@gmail.com>
2024-10-14 21:42:31 +0630

    add_action_menu

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,8 +4,10 @@
     "version": "17.0.0.0",
     "depends": ["base","crm"],
     "data":[
-        "security/res_groups.xml",
         "security/ir.model.access.csv",
+        "security/res_groups.xml",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
     "demo":[
         "demo/demo.xml",
